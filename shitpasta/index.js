0d22e2aac1887bf8a30ef1ac989bdc5daa7372f5
Eddcakes <eddadamson@gmail.com>
2020-03-21 23:56:38 +0000

    add node fetch and 1st reddit shitpost

--- shitpasta/index.js
+++ shitpasta/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -6,4 +7,50 @@ client.once("ready", () => {
   console.log("🤖 armed for shitposting 🤖");
 });
 
+const url = "https://www.reddit.com/r/copypasta/hot.json";
+
+client.on("message", msg => {
+  if (
+    msg.guild.id === process.env.SERVER_ID &&
+    msg.channel.id === process.env.CHANNEL_ID
+  ) {
+    if (msg.author.bot) return;
+    if (msg.content.startsWith("!shitpost")) {
+      spamage()
+        .then(content => {
+          /* 
+        ok so DiscordAPIError is thrown when body > 2000 chacters so need to check for this
+        tts only speaks a few sentences through
+        could we try breaking up into multiple messages to solve both of these?
+        */
+          return msg.channel.send(content, { tts: true });
+        })
+        .catch(
+          msg.channel.send("oooopsy whoopsy something went wrong 😢", {
+            tts: true
+          })
+        );
+    }
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
+
+function spamage() {
+  return fetch("https://www.reddit.com/r/copypasta/hot.json")
+    .then(response => response.json())
+    .then(call => {
+      const posts = call.data.children;
+      const rndIndex = Math.floor(
+        Math.random() * call.data.children.length - 1
+      );
+      const title = posts[rndIndex].data.title;
+      const selftext = posts[rndIndex].data.selftext;
+      if (title.length > selftext.length) {
+        return title;
+      } else {
+        return selftext;
+      }
+    })
+    .catch(err => console.log(err));
+}
