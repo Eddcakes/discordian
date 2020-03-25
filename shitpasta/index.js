d46775cd69b84f665f161460786d698e915276de
Eddcakes <eddadamson@gmail.com>
2020-03-25 22:46:10 +0000

    adds more config and dev funcs for easier debug

--- shitpasta/index.js
+++ shitpasta/index.js
@@ -2,10 +2,55 @@ require("dotenv").config();
 const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const env_settings = process.env.SETTINGS;
+const subReddit = process.env.SUB_REDDIT || "copypasta";
+
+client.on("ready", () => {
+  if (process.env.NODE_ENV === "production") {
+    client.user
+      .setActivity("the clock")
+      .then(presence =>
+        console.log(
+          `set for production activity ${presence.activities[0].name}`
+        )
+      )
+      .catch(console.error);
+  }
+
+  if (env_settings === "development") {
+    client.user
+      .setActivity("dev mode")
+      .then(presence =>
+        console.log(`set dev activity ${presence.activities[0].name}`)
+      )
+      .catch(console.error);
+  }
+});
 
 client.once("ready", () => {
   console.log("🤖 armed for shitposting 🤖");
 });
+
+const prefix = "!shitpost ";
+
+if (env_settings === "development") {
+  client.on("message", async msg => {
+    if (
+      msg.guild.id === process.env.SERVER_ID &&
+      msg.channel.id === process.env.CHANNEL_ID
+    ) {
+      if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+      const args = msg.content.slice(prefix.length).split(/ +/);
+      const postNo = parseInt(args.shift());
+      /* stickied posts don't seem to count towards the 25 default so can be higher than 25 */
+      if (Number.isInteger(postNo) && postNo < 25) {
+        ttsPost(msg, subReddit, postNo);
+      } else {
+        msg.reply(`argument must be a number < 25, you entered: ${postNo}`);
+      }
+    }
+  });
+}
 
 client.on("message", async msg => {
   if (
@@ -13,30 +58,35 @@ client.on("message", async msg => {
     msg.channel.id === process.env.CHANNEL_ID
   ) {
     if (msg.author.bot) return;
-    if (
-      msg.content.startsWith("!shitpost") ||
-      msg.content.startsWith("!shitpasta")
-      // if SHITPASTA could tocaps the message lol
-    ) {
-      await spamage("copypasta")
-        .then(content => {
-          /* 
-          now the message is chunked for tts it says the bot name each new chunk 
-          `beepboop said:` every 184 char chunk 
-        */
-          content.map(chunkedContent => {
-            msg.channel.send(chunkedContent, {
-              tts: true
-            });
-          });
-        })
-        .catch(err => console.log("spamage error: ", err));
+    if (msg.content.startsWith("!shitpasta")) {
+      ttsPost(msg, subReddit);
     }
   }
 });
+
 client.login(process.env.BOT_TOKEN);
 
-function spamage(subreddit) {
+/**
+ * wrapper to send spam as tts
+ * @param {Object} msg the discord message object so we can send the messages
+ * @param {String} sub subreddit
+ * @param {Number} postNumber post number, limit ~< 25 if null || omitted uses random num
+ */
+async function ttsPost(msg, sub, postNumber) {
+  await spamage(sub, postNumber)
+    .then(content => {
+      // now the message is chunked for tts it says the bot name each new chunk
+      // `beepboop said:` every 184 char chunk
+      content.map(chunkedContent => {
+        msg.channel.send(unescape(chunkedContent), {
+          tts: true
+        });
+      });
+    })
+    .catch(err => console.log(`ttsPost error: ${err}`));
+}
+
+function spamage(subreddit, postNumber = null) {
   return fetch(`https://www.reddit.com/r/${subreddit}/hot.json`)
     .then(response => response.json())
     .then(call => {
@@ -44,8 +94,14 @@ function spamage(subreddit) {
       const rndIndex = Math.floor(
         Math.random() * call.data.children.length - 1
       );
-      const title = posts[rndIndex].data.title;
-      const selftext = posts[rndIndex].data.selftext;
+      const title =
+        postNumber === null || postNumber < posts.length
+          ? posts[rndIndex].data.title
+          : posts[postNumber].data.title;
+      const selftext =
+        postNumber === null || postNumber < posts.length
+          ? posts[rndIndex].data.selftext
+          : posts[postNumber].data.selftext;
       if (title.length > selftext.length) {
         return chunk(title);
       } else {
@@ -53,7 +109,7 @@ function spamage(subreddit) {
       }
     })
     .catch(err => {
-      return console.log(err);
+      return console.log(`spamage ${err}`);
     });
 }
 
